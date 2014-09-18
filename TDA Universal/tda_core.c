91318ea3525e10d177560b20ff513c60510b0e34
CrSeth <sethstalley@gmail.com>
2014-09-17 18:37:54 -0600

    Actualizar TDA para copiar dato a memoria

--- TDA Universal/tda_core.c
+++ TDA Universal/tda_core.c
@@ -1,22 +1,17 @@
 #include "tda_core.h"
 
+static tda_base_t *tda_base_node(void *pData);
+static tda_base_t *tda_base_node_copy(void *data);
+
 void tda_set_indexes(tda_base_t **head)
 {
 	tda_base_t *tmp = *head;
 	int i = 1;
 
-	if((*head)->tda_type != TDA_CIRCULAR_LIST){
-		while(tmp){
-			tmp->tda_index = i;
-			tmp = tmp->tda_next;
-			i++;
-		}
-	} else {
-		do {
-			tmp->tda_index = i;
-			tmp = tmp->tda_next;
-			i++;
-		} while(tmp->tda_next != *head);
+	while(tmp){
+		tmp->tda_index = i;
+		tmp = tmp->tda_next;
+		i++;
 	}
 }
 
@@ -29,10 +24,30 @@ void tda_base_nnew(tda_base_t **head, int maxn)
 
 }
 
-tda_base_t *tda_base_node(void *data)
+/*retorna un nodo de la lista donde dato es guardado
+por referencia al data original */
+static tda_base_t *tda_base_node(void *data)
 {
 	tda_base_t *node = (tda_base_t *) malloc (sizeof(tda_base_t));
-	node->tda_data = data;
+	//obetner memoria para hacer una copia del dato generico
+	node-> dataCopied = 0;
+	node-> tda_data = data;
+	node->tda_next = NULL;
+	node->tda_last = NULL;
+	node->tda_index = TDA_INIT_INDEX;
+	tda_set_indexes(&node);
+	return node;
+}
+
+/*retorna un nodo de la lista donde el dato es copiado
+al heap y un puntero a tal es guardado en dato */
+static tda_base_t *tda_base_node_copy(void *data)
+{
+	tda_base_t *node = (tda_base_t *) malloc (sizeof(tda_base_t));
+	//obetner memoria para hacer una copia del dato generico
+	node-> dataCopied = 1;
+	node-> tda_data = malloc(sizeof(data));
+	memcpy(node->tda_data, data, sizeof(data));	
 	node->tda_next = NULL;
 	node->tda_last = NULL;
 	node->tda_index = TDA_INIT_INDEX;
@@ -58,21 +73,36 @@ int tda_base_add(tda_base_t **head, void *data)
 	return index;
 }
 
+//agrega un elemento copiando ese elento a memoria
+int tda_base_add_copy (tda_base_t **head, void *data){
+	tda_base_t *tmp=NULL;
+	int index = 0;
+	if(!(*head)){
+		*head = tda_base_node_copy(data);
+		index = 1;
+	} else {
+		tmp = *head;
+		while(tmp->tda_next) tmp = tmp->tda_next;
+		tmp->tda_next = tda_base_node_copy(data);
+		tmp->tda_next->tda_last = tmp;
+		tda_set_indexes(head);
+		index = tmp->tda_next->tda_index;
+	}
+	return index;
+
+}
+
 int tda_base_insf(tda_base_t **head, void *data)
 {
-	
-	if( *head && (*head)->tda_type == TDA_CIRCULAR_LIST) return -1; 
+	if( (*head)->tda_type != TDA_BASE || (*head)->tda_type != TDA_SIMPLE_LIST) return -1;
 	if(!(*head)){
-		
 		*head = tda_base_node(data);
 	} else {
-		
 		tda_base_t *tmp = *head;
 		tmp->tda_last = tda_base_node(data);
 		tmp->tda_last->tda_next = *head;
 		*head = tmp->tda_last;
 	}
-	
 	tda_set_indexes(head);
 	return (*head)->tda_index;
 }
@@ -100,7 +130,7 @@ tda_base_t *tda_base_search(tda_base_t **head, int index)
 	int starting = 1;
 	tda_base_t *tmp = *head;
 	if(tmp->tda_type != TDA_CIRCULAR_LIST){
-		while(tmp && tmp->tda_index != index)
+		while(tmp->tda_index != index)
 			tmp = tmp->tda_next;
 	} else {
 		while( ((tmp != *head && starting == 0) || (tmp == *head && starting != 0)) && tmp->tda_index != index){
@@ -124,22 +154,32 @@ void *tda_base_getdata(tda_base_t **head, int index)
 }
 
 static void tda_base_del(tda_base_t **head,tda_base_t **dnode){
+	//printf("call tda_base_del ... \n");
 	tda_base_t *tmp = *dnode;
 	if(!(*head) || !(*dnode)) return;
 	else if(*head && !((*head)->tda_last) && !((*head)->tda_next))
 		*head = NULL;
 	else if(!tmp->tda_last && tmp->tda_next){
 		(*head) = tmp->tda_next;
+		//si dato fue copiado
+		if(tmp->dataCopied)
+			free(tmp->tda_data);
 		free(tmp);
+
 	} else if(tmp->tda_last && !tmp->tda_next){
 		tmp->tda_last->tda_next = NULL;
+		//si dato fue copiado
+		if(tmp->dataCopied)
+			free(tmp->tda_data);
 		free(tmp);
 	} else {
 		(tmp)->tda_last->tda_next = (tmp)->tda_next;
 		(tmp)->tda_next->tda_last = (tmp->tda_last);
 		tmp=NULL;
 	}
-	if(*head) tda_set_indexes(head);
+	if(*head){//printf("Rebuilding indexes ... \n");
+	 tda_set_indexes(head);}
+	//printf("tda_base_del ret \n");
 }
 
 void tda_base_delete(tda_base_t **head, int index)
